87f5e54d28d25384af89fdc0126d912ade9fdbd6
Archana S <archana.s@ibm.com>
2021-09-22 20:10:47 +0530

    spring

---
+++ KeepNote1/src/main/java/com/stackroute/keepnote/model/Note.java
@@ -0,0 +1,88 @@
+package com.stackroute.keepnote.model;
+
+import java.time.LocalDateTime;
+
+/*
+ * The class "Note" will be acting as the data model for the Note data in the ArrayList.
+ */
+public class Note {
+	private int noteId;	private String noteTitle;	
+	private String noteContent;	
+	private String noteStatus;	
+	private LocalDateTime createdAt;
+	/*
+	 * This class should have five fields (noteId, noteTitle, noteContent,
+	 * noteStatus and createdAt). This class should also contain the getters and
+	 * setters for the fields. The value of createdAt should not be accepted from
+	 * the user but should be always initialized with the system date
+	 */
+
+	public Note() {
+		
+	}
+
+	public Note(int noteId, String noteTitle, String noteContent, String noteStatus, LocalDateTime createdAt) {
+		super();
+		this.noteId = noteId;
+		this.noteTitle = noteTitle;
+		this.noteContent = noteContent;
+		this.noteStatus = noteStatus;
+		this.createdAt = createdAt;
+	}
+
+	/* All the getters/setters definition should be implemented here */
+
+	public int getNoteId() {
+		return noteId;
+
+	}
+
+	public void setNoteId(int intid) {
+		this.noteId = intid;
+
+	}
+
+	public String getNoteTitle() {
+		return noteTitle;
+	}
+
+	public void setNoteTitle(String string) {
+		
+		this.noteTitle = string;
+
+	}
+
+	public String getNoteContent() {
+		return noteContent;
+	}
+
+	public void setNoteContent(String string) {
+		this.noteContent = string;
+
+	}
+
+	public String getNoteStatus() {
+		return noteStatus;
+	}
+
+	public void setNoteStatus(String string) {
+		this.noteStatus = string;
+
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime localdatetime) {
+		this.createdAt = localdatetime;
+
+	}
+
+	/* Override the toString() method */
+
+	@Override
+	public String toString() {
+		return null;
+	}
+}
